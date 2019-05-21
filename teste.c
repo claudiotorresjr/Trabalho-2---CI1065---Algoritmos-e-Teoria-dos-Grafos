ab9488092911f36e01bcbe0fbf42ff6edf23f42f
ctj17 <ctj17@inf.ufpr.br>
2019-05-21 01:41:06 -0300

    falta ajustar

--- teste.c
+++ teste.c
@@ -11,12 +11,14 @@ int main(void) {
 
     return 1;
 	
-	vertice **cobertura = NULL;
+	vertice **cobertura;
 
 	g = escreve_grafo(stdout, g);
 	unsigned int k;
-	k = cobertura_por_trilhas(g, cobertura);
+	k = cobertura_por_trilhas(g, &cobertura);
     printf ("K == %u\n", k);
+
+    mostraCobertura(cobertura, k);
 
     return ! destroi_grafo(g);
 }
